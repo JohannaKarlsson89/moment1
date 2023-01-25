a8b0f113831d8fbfa3d2cbd8322afc0beae4f4cb
Johanna Karlsson <joka2113@student.miun.se>
2023-01-25 18:46:46 +0100

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var about = document.getElementsByClassName("about");
+var i;
+
+for (i = 0; i < about.length; i++) {
+  about[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var section = this.nextElementSibling;
+    if (section.style.display === "block") {
+      section.style.display = "none";
+    } else {
+      section.style.display = "block";
+    }
+  });
+}
